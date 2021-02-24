b6d1ea23d6b416f70801cf8ce3d45c2a055c8b31
Igor.Safiuillin <igor.safiullin@innostage-group.ru>
2021-02-25 02:39:28 +0300

    Start компонент

---
+++ src/components/EnterName.vue
@@ -0,0 +1,40 @@
+<template>
+<v-container class="d-flex flex-column justify-center align-center mt-11">
+    <h3 class="asteria__welcome mb-3">Здравствуйте</h3>
+    <h2 class="asteria__headline mb-6">Как Вас зовут?</h2>
+    <img :src="require('@/assets/img/start/saturn.svg')"alt="">
+    <v-text-field
+            placeholder="Имя"
+            v-model="name"
+            outlined
+            class="asteria__input mt-8"
+            color="#FF8868"
+            height="48px"
+    />
+    <div class="button-center d-flex justify-center">
+        <v-btn
+                rounded
+                class="asteria__button"
+        >
+            Продолжить
+        </v-btn>
+    </div>
+</v-container>
+</template>
+
+<script>
+export default {
+    name: "EnterName",
+    data: () => ({
+        name: ''
+    })
+}
+</script>
+
+<style lang="scss" scoped>
+.button-center {
+    width: 100%;
+    position: absolute;
+    bottom: 22px;
+}
+</style>
---
+++ src/views/Start.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="darkula">
+    <EnterName v-if="step === 1" />
+</div>
+</template>
+
+<script>
+import EnterName from "../components/EnterName";
+export default {
+    name: "Start",
+
+    components: {EnterName},
+    data: () => ({
+       step: 1,
+    }),
+}
+</script>
+
+<style scoped>
+
+</style>
